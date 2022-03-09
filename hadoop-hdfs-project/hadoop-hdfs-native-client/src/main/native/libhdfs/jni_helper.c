74623acfc1c4c1c3dbf03ae2ad0c4976a60b18c2
William Montaz <w.montaz@criteo.com>
2022-03-09 15:29:18 +0100

    Revert adding LIBHDFS_CLASSPATH variable
    Using a glob classpath for native hdfs client is not required anymore

--- hadoop-hdfs-project/hadoop-hdfs-native-client/src/main/native/libhdfs/jni_helper.c
+++ hadoop-hdfs-project/hadoop-hdfs-native-client/src/main/native/libhdfs/jni_helper.c
@@ -679,14 +679,11 @@ static JNIEnv* getGlobalJNIEnv(void)
 
     if (noVMs == 0) {
         //Get the environment variables for initializing the JVM
-        hadoopClassPath = getenv("LIBHDFS_CLASSPATH");
+        hadoopClassPath = getClassPath();
         if (hadoopClassPath == NULL) {
-          hadoopClassPath = getenv("CLASSPATH");
-          if (hadoopClassPath == NULL) {
-            fprintf(stderr, "Environment variables LIBHDFS_CLASSPATH or CLASSPATH not set!\n");
+            fprintf(stderr, "Environment variable CLASSPATH not set!\n");
             return NULL;
-          }
-        }
+        } 
         optHadoopClassPathLen = strlen(hadoopClassPath) + 
           strlen(hadoopClassPathVMArg) + 1;
         optHadoopClassPath = malloc(sizeof(char)*optHadoopClassPathLen);
