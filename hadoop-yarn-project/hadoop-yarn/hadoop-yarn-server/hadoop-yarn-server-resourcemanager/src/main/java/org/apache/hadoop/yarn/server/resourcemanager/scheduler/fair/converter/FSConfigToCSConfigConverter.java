6f21c56aae75df25ec25b581489815cb59c96b3b
PJ Fanning <pjfanning@users.noreply.github.com>
2024-03-29 04:17:18 +0100

    HADOOP-19041. Use StandardCharsets in more places (#6449)
    (cherry picked from commit 97c5a6efbaf01dcab55fdff4d24fe59b6cda1c99)

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/fair/converter/FSConfigToCSConfigConverter.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/fair/converter/FSConfigToCSConfigConverter.java
@@ -345,7 +345,7 @@ public class FSConfigToCSConfigConverter {
       if (!rulesToFile) {
         String json =
             ((ByteArrayOutputStream)mappingRulesOutputStream)
-            .toString(StandardCharsets.UTF_8.displayName());
+            .toString(StandardCharsets.UTF_8.name());
         capacitySchedulerConfig.setMappingRuleJson(json);
       }
     } else {
