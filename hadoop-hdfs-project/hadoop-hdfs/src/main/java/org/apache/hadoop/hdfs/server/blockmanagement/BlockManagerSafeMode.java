87cbb90c30804ad3754c6c9b817f05ff81eb3a36
w.montaz <w.montaz@criteo.com>
2025-03-11 16:57:57 +0100

    [CRITEO][HDFS-13295] Fix wrong count of block replicas during restart of standby preventing to leave safe mode
    (cherry picked from commit 33accbc5c901eebe99e1d3c9fe746977e8ce765a)

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/blockmanagement/BlockManagerSafeMode.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/blockmanagement/BlockManagerSafeMode.java
@@ -531,6 +531,10 @@ class BlockManagerSafeMode {
     return bytesInFutureECBlockGroups.longValue();
   }
 
+  public int getSafeReplication() {
+    return safeReplication;
+  }
+
   void close() {
     assert namesystem.hasWriteLock() : "Closing bmSafeMode needs write lock!";
     try {
