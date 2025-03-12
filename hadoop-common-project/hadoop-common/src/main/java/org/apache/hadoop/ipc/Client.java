a4040946c46b256b166f28a9e9aedcba204818e2
w.montaz <w.montaz@criteo.com>
2025-03-12 16:27:51 +0100

    [CRITEO] Prevent ipc.Client to log StandbyException at WARN level

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/ipc/Client.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/ipc/Client.java
@@ -729,6 +729,9 @@ public class Client implements AutoCloseable {
             // and should not print a warning log.
             if (ex instanceof InterruptedIOException) {
               LOG.debug("Exception encountered while connecting to the server {}", remoteId, ex);
+            } else if (ex instanceof StandbyException) {
+              LOG.debug("Exception encountered while connecting to the server",
+                    ex);
             } else {
               LOG.warn("Exception encountered while connecting to the server {}", remoteId, ex);
             }
