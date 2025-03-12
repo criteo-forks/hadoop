c5f011a3c70ddf1660971a2c50aba5a69766f576
w.montaz <w.montaz@criteo.com>
2025-03-12 15:32:33 +0100

    [CRITEO] Add application's live containers endpoint on yarn resourcemanager's Rest API

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/AbstractYarnScheduler.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/AbstractYarnScheduler.java
@@ -1472,6 +1472,11 @@ public abstract class AbstractYarnScheduler
         rmContext, demotedContainer, false);
   }
 
+  @Override
+  public Collection<RMContainer> getLiveContainers(ApplicationAttemptId applicationAttemptId) {
+    return getApplicationAttempt(applicationAttemptId).getLiveContainers();
+  }
+
   /**
    * Rollback container update after expiry.
    * @param containerId ContainerId.
