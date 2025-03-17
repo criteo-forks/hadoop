9045606ec396ac6cd1b3cb75a1c310817c0b0b59
w.montaz <w.montaz@criteo.com>
2025-03-17 14:25:18 +0100

    [CRITEO] Add rpc metric for enqueue time
    Enqueue time is measured, subtracted to get the queue time but not shown in the metrics. In the case of big enqueue time, we could see small queue time, small processing time and clients would still suffer a bad experience that we could not measure server side.

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/ipc/metrics/RpcMetrics.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/ipc/metrics/RpcMetrics.java
@@ -71,6 +71,8 @@ public class RpcMetrics {
     if (rpcQuantileEnable) {
       rpcQueueTimeQuantiles =
           new MutableQuantiles[intervals.length];
+      rpcEnqueueTimeQuantiles =
+          new MutableQuantiles[intervals.length];
       rpcLockWaitTimeQuantiles =
           new MutableQuantiles[intervals.length];
       rpcProcessingTimeQuantiles =
@@ -81,6 +83,9 @@ public class RpcMetrics {
         int interval = intervals[i];
         rpcQueueTimeQuantiles[i] = registry.newQuantiles("rpcQueueTime"
             + interval + "s", "rpc queue time in " + metricsTimeUnit, "ops",
+            "latency", interval);
+        rpcEnqueueTimeQuantiles[i] = registry.newQuantiles("rpcEnqueueTime"
+                + interval + "s", "rpc enqueue time in " + metricsTimeUnit , "ops",
             "latency", interval);
         rpcLockWaitTimeQuantiles[i] = registry.newQuantiles(
             "rpcLockWaitTime" + interval + "s",
@@ -110,6 +115,8 @@ public class RpcMetrics {
   @Metric("Number of sent bytes") MutableCounterLong sentBytes;
   @Metric("Queue time") MutableRate rpcQueueTime;
   MutableQuantiles[] rpcQueueTimeQuantiles;
+  @Metric("Enqueue time") MutableRate rpcEnqueueTime;
+  MutableQuantiles[] rpcEnqueueTimeQuantiles;
   @Metric("Lock wait time") MutableRate rpcLockWaitTime;
   MutableQuantiles[] rpcLockWaitTimeQuantiles;
   @Metric("Processing time") MutableRate rpcProcessingTime;
@@ -248,6 +255,15 @@ public class RpcMetrics {
     rpcQueueTime.add(qTime);
     if (rpcQuantileEnable) {
       for (MutableQuantiles q : rpcQueueTimeQuantiles) {
+        q.add(qTime);
+      }
+    }
+  }
+
+  public void addRpcEnqueueTime(long qTime) {
+    rpcEnqueueTime.add(qTime);
+    if (rpcQuantileEnable) {
+      for (MutableQuantiles q : rpcEnqueueTimeQuantiles) {
         q.add(qTime);
       }
     }
