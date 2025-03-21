c8289fd547206d92066546b57d8d1e3475b6cb2b
w.montaz <w.montaz@criteo.com>
2025-03-21 15:29:42 +0100

    [CRITEO] Allow to exclude some block pool ids from transfer and write throttling
    This is particularly useful when some nameservices are spread accross several datacenters and thus require throttling but some are not spread and should not account in throttling decisions.

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/DFSConfigKeys.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/DFSConfigKeys.java
@@ -119,10 +119,14 @@ public class DFSConfigKeys extends CommonConfigurationKeys {
       "dfs.datanode.data.transfer.bandwidthPerSec";
   public static final long DFS_DATANODE_DATA_TRANSFER_BANDWIDTHPERSEC_DEFAULT =
       0; // A value of zero indicates no limit
+  public static final String DFS_DATANODE_DATA_TRANSFER_THROTTLER_EXCLUDED_BLOCK_POOL_IDS_KEY =
+      "dfs.datanode.data.transfer.throttler.excluded-block-pool-ids";
   public static final String DFS_DATANODE_DATA_WRITE_BANDWIDTHPERSEC_KEY =
       "dfs.datanode.data.write.bandwidthPerSec";
   // A value of zero indicates no limit
   public static final long DFS_DATANODE_DATA_WRITE_BANDWIDTHPERSEC_DEFAULT = 0;
+  public static final String DFS_DATANODE_DATA_WRITE_THROTTLER_EXCLUDED_BLOCK_POOL_IDS_KEY =
+          "dfs.datanode.data.write.throttler.excluded-block-pool-ids";
   public static final String DFS_DATANODE_EC_RECONSTRUCT_READ_BANDWIDTHPERSEC_KEY =
       "dfs.datanode.ec.reconstruct.read.bandwidthPerSec";
   public static final long DFS_DATANODE_EC_RECONSTRUCT_READ_BANDWIDTHPERSEC_DEFAULT =
