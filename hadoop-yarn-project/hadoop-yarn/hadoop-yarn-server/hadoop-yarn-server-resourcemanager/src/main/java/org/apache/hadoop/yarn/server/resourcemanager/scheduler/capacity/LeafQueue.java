2d8f96925991fd57489ad6e339b2cfebab0511b1
w.montaz <w.montaz@criteo.com>
2025-03-21 16:06:06 +0100

    [CRITEO] Add new property to enforce AM resource limit check per queue/user for first app
    * Add new property to control skipping am limit enforcement for first app
    
     - LeafQueue checks that the limit for AM is not
       reached for the given queue before accepting the
       app, but it allows at least one app to be scheduled
       even if it doesn't fit in this limit.
     - This can be problematic when using GPU as resource
       as most of the queue we have doesn't allow usage of GPU
       but it will accept the first app submitted requesting GPU
       and this will block all future applications since the AM
       limit is reached as PendingGPUs > 0 ( limit for GPU in this queue)
     - The new property is set to true by default and can be updated with:
       "yarn.scheduler.capacity.%s.skip-am-limit-enforcement-for-first-app"
       in the capacity-scheduler.xml file. if set to false the first app
       will be checked against the AM resource limit and will be deactivated
       with a "Queue's AM resource limit exceeded." error msg.
    
    * Fix skip condition to handle empty AM resource usage
    
     * The limit on the AM resources usage per queue/user
       check is skipped when there is no active apps or
       the AM resources usage is 0. Update the condition
       to cover both cases.
    
    * Add inheritance for skip-am-limit-for-first-app property
    
     - if no config is specified for a given queue it
       will inherit its parent value.

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/capacity/LeafQueue.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/capacity/LeafQueue.java
@@ -92,6 +92,8 @@ public class LeafQueue extends AbstractCSQueue {
   
   private float maxAMResourcePerQueuePercent;
 
+  private boolean skipAmLimitForFirstApp;
+
   private volatile int nodeLocalityDelay;
   private volatile int rackLocalityAdditionalDelay;
   private volatile boolean rackLocalityFullReset;
@@ -220,6 +222,8 @@ public class LeafQueue extends AbstractCSQueue {
       maxAMResourcePerQueuePercent =
           conf.getMaximumApplicationMasterResourcePerQueuePercent(
               getQueuePath());
+
+      skipAmLimitForFirstApp = conf.getSkipAmLimitForFirstApp(getQueuePath(), parent);
 
       priorityAcls = conf.getPriorityAcls(getQueuePath(),
           scheduler.getMaxClusterLevelAppPriority());
@@ -841,7 +845,7 @@ public class LeafQueue extends AbstractCSQueue {
         }
 
         if (!resourceCalculator.fitsIn(amIfStarted, amLimit)) {
-          if (getNumActiveApplications() < 1 || (Resources.lessThanOrEqual(
+          if (skipAmLimitForFirstApp && (getNumActiveApplications() < 1 || Resources.lessThanOrEqual(
               resourceCalculator, lastClusterResource,
               queueUsage.getAMUsed(partitionName), Resources.none()))) {
             LOG.warn("maximum-am-resource-percent is insufficient to start a"
@@ -873,7 +877,7 @@ public class LeafQueue extends AbstractCSQueue {
             user.getConsumedAMResources(partitionName));
 
         if (!resourceCalculator.fitsIn(userAmIfStarted, userAMLimit)) {
-          if (getNumActiveApplications() < 1 || (Resources.lessThanOrEqual(
+          if (skipAmLimitForFirstApp && (getNumActiveApplications() < 1 || Resources.lessThanOrEqual(
               resourceCalculator, lastClusterResource,
               queueUsage.getAMUsed(partitionName), Resources.none()))) {
             LOG.warn("maximum-am-resource-percent is insufficient to start a"
