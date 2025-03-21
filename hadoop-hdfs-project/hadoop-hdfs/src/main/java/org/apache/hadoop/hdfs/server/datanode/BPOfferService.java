e09036bf9b52486e3d3167f33939910d7df42ed2
w.montaz <w.montaz@criteo.com>
2025-03-21 15:31:56 +0100

    [CRITEO] The namenode setBandwidth command can update balance, trasnfer and write throttlers independently
    Details: https://criteo.atlassian.net/browse/LAKE-15901
    (cherry picked from commit 56fa2e89511a6536be6bf467301db3a571fc4e32)

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/BPOfferService.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/BPOfferService.java
@@ -17,7 +17,9 @@
  */
 package org.apache.hadoop.hdfs.server.datanode;
 
+import java.util.function.Function;
 import org.apache.hadoop.classification.VisibleForTesting;
+import org.apache.hadoop.hdfs.util.DataTransferThrottler;
 import org.apache.hadoop.util.Preconditions;
 
 import org.apache.hadoop.classification.InterfaceAudience;
@@ -789,15 +791,16 @@ class BPOfferService {
       break;
     case DatanodeProtocol.DNA_BALANCERBANDWIDTHUPDATE:
       LOG.info("DatanodeCommand action: DNA_BALANCERBANDWIDTHUPDATE");
-      long bandwidth =
+      long encodedBandwidth =
                  ((BalancerBandwidthCommand) cmd).getBalancerBandwidthValue();
-      if (bandwidth > 0) {
+      if (encodedBandwidth > 0) {
         DataXceiverServer dxcs =
                      (DataXceiverServer) dn.dataXceiverServer.getRunnable();
-        LOG.info("Updating balance throttler bandwidth from "
-            + dxcs.balanceThrottler.getBandwidth() + " bytes/s "
-            + "to: " + bandwidth + " bytes/s.");
-        dxcs.balanceThrottler.setBandwidth(bandwidth);
+        
+        List<Throttler> throttlers = Throttler.decodeThrottler(encodedBandwidth);
+        for (Throttler throttler : throttlers) {
+          throttler.setBandwidth(dxcs, encodedBandwidth);
+        }
       }
       break;
     case DatanodeProtocol.DNA_ERASURE_CODING_RECONSTRUCTION:
@@ -811,7 +814,64 @@ class BPOfferService {
     }
     return true;
   }
- 
+  
+  /**
+   *
+   * Utility enum for DatanodeProtocol.DNA_BALANCERBANDWIDTHUPDATE
+   *
+   * 3 bandwidth values for the 3 throttlers are encoded in the smallest 39 bits.
+   * This is because max bandwidth is limited by HdfsServerConstants.MAX_BANDWIDTH_PER_DATANODE which is 1TB/s
+   *
+   * The unit is MB/sec (as we are not interested in throttling below 1MB/sec).
+   *
+   * Thanks to that each throttler has 13 bits available to encode its bandwidth value, up to 8191 MB/s
+   */
+
+  enum Throttler {
+    BALANCE(0, 0x1FFF, "balance", dxcs -> dxcs.balanceThrottler),
+    TRANSFER(13, 0x1FFF, "transfer", dxcs -> dxcs.getTransferThrottler(null)),
+    WRITE(26, 0x1FFF, "write", dxcs -> dxcs.getWriteThrottler(null));
+  
+    private final int shift;
+    private final long mask;
+    private final String name;
+    private final Function<DataXceiverServer, DataTransferThrottler> selectThrottlerStrategy;
+
+    Throttler(int shift, long mask, String name, Function<DataXceiverServer, DataTransferThrottler> selectThrottlerStrategy) {
+      this.shift = shift;
+      this.mask = mask;
+      this.name = name;
+      this.selectThrottlerStrategy = selectThrottlerStrategy;
+    }
+    
+    static List<Throttler> decodeThrottler(long encodedBandwidthValue) {
+      List<Throttler> selectedThrottlers = new ArrayList<>();
+      for (Throttler throttler : Throttler.values()) {
+        if ((encodedBandwidthValue >>> throttler.shift & throttler.mask) != 0) {
+          selectedThrottlers.add(throttler);
+        }
+      }
+      return selectedThrottlers;
+    }
+  
+    public void setBandwidth(DataXceiverServer dxcs, long encodedBandwidth) {
+      // Bandwidth is encoded in MB/sec
+      long bandwidth = decodeBandwidth(encodedBandwidth) * 1024 * 1024;
+      DataTransferThrottler throttler = selectThrottlerStrategy.apply(dxcs);
+      if (throttler != null) {
+        LOG.info("Updating " + name + " throttler bandwidth from "
+            + throttler.getBandwidth() + " bytes/s "
+            + "to: " + bandwidth + " bytes/s.");
+        throttler.setBandwidth(bandwidth);
+      }
+    }
+  
+    @VisibleForTesting
+    public long decodeBandwidth(long encodedBandwidthValue) {
+      return encodedBandwidthValue >>> shift & mask;
+    }
+  }
+  
   /**
    * This method should handle commands from Standby namenode except
    * DNA_REGISTER which should be handled earlier itself.
