9a1dc553aca121a48974c16a3a60d2fe0cbef91c
w.montaz <w.montaz@criteo.com>
2025-03-25 10:09:17 +0100

    [CRITEO] All included NM appear as NEW at startup
    Then if they reconnect they will be running, and if not they eventually become LOST because the NMLivelinessMonitor will fire an EXPIRE event.
    
    If LOST state was the first state of any nodemanager, it would trigger an immediate decommission if the nodes are under a maintenance.
    Thus instead of directly calling the transition to LOST, we let the NMLivelinessMonitor do the job. This way we have the same timeout to consider NM LOST.
    
    Putting the NM in the active NM set at startup causes many issues because a lot of components try to perform operations on this active set, thus it is easier to put them in the inactive set.
    However, doing so, the EXPIRE event triggered by NMLivelinessMonitor cannot reach the RMNode because the dispatcher only transmit events to RMNode in the active set. A small change to the dispatcher helps to cope with this.
    
    When upgrading to 3.5, check the behavior of https://issues.apache.org/jira/browse/YARN-11730

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/rmnode/RMNodeImpl.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/rmnode/RMNodeImpl.java
@@ -224,6 +224,9 @@ public class RMNodeImpl implements RMNode, EventHandler<RMNodeEvent> {
       .addTransition(NodeState.NEW, NodeState.NEW,
           RMNodeEventType.FINISHED_CONTAINERS_PULLED_BY_AM,
           new AddContainersToBeRemovedFromNMTransition())
+      .addTransition(NodeState.NEW, NodeState.LOST,
+          RMNodeEventType.EXPIRE,
+          new DeactivateNodeTransition(NodeState.LOST))
 
       //Transitions from RUNNING state
       .addTransition(NodeState.RUNNING,
@@ -785,28 +788,32 @@ public class RMNodeImpl implements RMNode, EventHandler<RMNodeEvent> {
     ClusterMetrics metrics = ClusterMetrics.getMetrics();
     // Update utilization metrics
     this.updateClusterUtilizationMetrics();
+    decrementMetricBasedOnPreviousNodeState(previousNodeState);
+  }
 
+  private void decrementMetricBasedOnPreviousNodeState(NodeState previousNodeState) {
+    ClusterMetrics metrics = ClusterMetrics.getMetrics();
     switch (previousNodeState) {
-    case LOST:
-      metrics.decrNumLostNMs();
-      break;
-    case REBOOTED:
-      metrics.decrNumRebootedNMs();
-      break;
-    case DECOMMISSIONED:
-      metrics.decrDecommisionedNMs();
-      break;
-    case UNHEALTHY:
-      metrics.decrNumUnhealthyNMs();
-      break;
-    case SHUTDOWN:
-      metrics.decrNumShutdownNMs();
-      break;
-    case DECOMMISSIONING:
-      metrics.decrDecommissioningNMs();
-      break;
-    default:
-      LOG.debug("Unexpected previous node state");
+      case LOST:
+        metrics.decrNumLostNMs();
+        break;
+      case REBOOTED:
+        metrics.decrNumRebootedNMs();
+        break;
+      case DECOMMISSIONED:
+        metrics.decrDecommisionedNMs();
+        break;
+      case UNHEALTHY:
+        metrics.decrNumUnhealthyNMs();
+        break;
+      case SHUTDOWN:
+        metrics.decrNumShutdownNMs();
+        break;
+      case DECOMMISSIONING:
+        metrics.decrDecommissioningNMs();
+        break;
+      default:
+        LOG.debug("Unexpected previous node state");
     }
   }
 
@@ -948,7 +955,9 @@ public class RMNodeImpl implements RMNode, EventHandler<RMNodeEvent> {
         previousRMNode =
             rmNode.context.getInactiveRMNodes().remove(unknownNodeId);
         if (previousRMNode != null) {
-          ClusterMetrics.getMetrics().decrDecommisionedNMs();
+          rmNode.decrementMetricBasedOnPreviousNodeState(previousRMNode.getState());
+          // The NM might have been tracked since startup, clean it
+          rmNode.context.getResourceTrackerService().getNMLivelinessMonitor().unregister(unknownNodeId);
         }
         containers = startEvent.getNMContainerStatuses();
         if (containers != null && !containers.isEmpty()) {
