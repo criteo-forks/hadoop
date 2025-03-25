5026a4eb18a7cb9b9b805aa47e007b5da83860e3
w.montaz <w.montaz@criteo.com>
2025-03-25 10:58:49 +0100

    [CRITEO] Force CachingStrategy in DataXceiver to derive from datanode configuration
    Client can pilot the behavior of datanode vfs caching via dfs.client.cache.drop.behind.writes, dfs.client.cache.drop.behind.reads, dfs.client.cache.readahead, however we want as administrators to be able to decide what is the datanode behavior regarding vfs cache.
    
    This commit forces cachingStrategy to use the datanode parameters regardless of the one provided by the client when used in DataXceiver.
    
    (cherry picked from commit 611726e55bde8536c21b6610b9ef8d42fdc0a843)

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/DNConf.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/DNConf.java
@@ -104,6 +104,7 @@ public class DNConf {
   final boolean overwriteDownstreamDerivedQOP;
   private final boolean pmemCacheRecoveryEnabled;
 
+  final boolean useDatanodeCachingStrategies;
   final long readaheadLength;
   final long heartBeatInterval;
   private final long lifelineIntervalMs;
@@ -297,6 +298,11 @@ public class DNConf {
         DFS_DATANODE_PROCESS_COMMANDS_THRESHOLD_KEY,
         DFS_DATANODE_PROCESS_COMMANDS_THRESHOLD_DEFAULT,
         TimeUnit.MILLISECONDS
+    );
+
+    this.useDatanodeCachingStrategies = getConf().getBoolean(
+        DFSConfigKeys.DFS_DATANODE_CACHE_USE_DATANODE_CACHING_STRATEGIES_KEY,
+        DFSConfigKeys.DFS_DATANODE_CACHE_USE_DATANODE_CACHING_STRATEGIES_DEFAULT
     );
   }
 
