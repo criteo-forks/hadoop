d42d9c0d2cd0b05e5b77edfa0e525bee901e36e8
w.montaz <w.montaz@criteo.com>
2025-03-25 10:59:16 +0100

    [CRITEO] Add a logger in BlockManager that shows for how long replication work computation hold the namesystem write lock
    This will be useful to increase decommission speed (sending more tasks to datanodes within a heartbeat period) while still making sure that client  performance is not too much impacted.
    
    (cherry picked from commit a1b6dd2645b6c39542cbeb822611b8d0d8b816aa)

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/blockmanagement/BlockManager.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/blockmanagement/BlockManager.java
@@ -168,6 +168,7 @@ public class BlockManager implements BlockStatsMXBean {
 
   public static final Logger LOG = LoggerFactory.getLogger(BlockManager.class);
   public static final Logger blockLog = NameNode.blockStateChangeLog;
+  public static final Logger replicationWorkTimingLog = LoggerFactory.getLogger("ReplicationWorkTiming");
 
   private static final String QUEUE_REASON_CORRUPT_STATE =
     "it has the wrong state or generation stamp";
@@ -2100,8 +2101,14 @@ public class BlockManager implements BlockStatsMXBean {
    *         iteration.
    */
   int computeBlockReconstructionWork(int blocksToProcess) {
+    long start = 0;
     List<List<BlockInfo>> blocksToReconstruct = null;
     namesystem.writeLock();
+
+    if (replicationWorkTimingLog.isDebugEnabled()) {
+      start = System.nanoTime();
+    }
+
     try {
       boolean reset = false;
       if (replQueueResetToHeadThreshold > 0) {
@@ -2117,6 +2124,11 @@ public class BlockManager implements BlockStatsMXBean {
           .chooseLowRedundancyBlocks(blocksToProcess, reset);
     } finally {
       namesystem.writeUnlock("computeBlockReconstructionWork");
+
+      if (replicationWorkTimingLog.isDebugEnabled()) {
+        replicationWorkTimingLog.debug("ReplicationWork for {} blocks. Hold write lock for {} nanos in computeBlockReconstructionWork",
+                blocksToProcess, System.nanoTime() - start);
+      }
     }
     return computeReconstructionWorkForBlocks(blocksToReconstruct);
   }
@@ -2131,11 +2143,17 @@ public class BlockManager implements BlockStatsMXBean {
   @VisibleForTesting
   int computeReconstructionWorkForBlocks(
       List<List<BlockInfo>> blocksToReconstruct) {
+    long start = 0;
     int scheduledWork = 0;
     List<BlockReconstructionWork> reconWork = new ArrayList<>();
 
     // Step 1: categorize at-risk blocks into replication and EC tasks
     namesystem.writeLock();
+
+    if (replicationWorkTimingLog.isDebugEnabled()) {
+      start = System.nanoTime();
+    }
+
     try {
       synchronized (neededReconstruction) {
         for (int priority = 0; priority < blocksToReconstruct
@@ -2151,6 +2169,11 @@ public class BlockManager implements BlockStatsMXBean {
       }
     } finally {
       namesystem.writeUnlock("computeReconstructionWorkForBlocks");
+
+      if (replicationWorkTimingLog.isDebugEnabled()) {
+        replicationWorkTimingLog.debug("ReplicationWork... Hold write lock for {} nanos in computeReconstructionWorkForBlocks Step-1",
+                System.nanoTime() - start);
+      }
     }
 
     // Step 2: choose target nodes for each reconstruction task
@@ -2176,6 +2199,11 @@ public class BlockManager implements BlockStatsMXBean {
 
     // Step 3: add tasks to the DN
     namesystem.writeLock();
+
+    if (replicationWorkTimingLog.isDebugEnabled()) {
+      start = System.nanoTime();
+    }
+
     try {
       for (BlockReconstructionWork rw : reconWork) {
         final DatanodeStorageInfo[] targets = rw.getTargets();
@@ -2192,6 +2220,11 @@ public class BlockManager implements BlockStatsMXBean {
       }
     } finally {
       namesystem.writeUnlock("computeReconstructionWorkForBlocks");
+
+      if (replicationWorkTimingLog.isDebugEnabled()) {
+        replicationWorkTimingLog.debug("ReplicationWork... Hold write lock for {} nanos in computeReconstructionWorkForBlocks Step-3",
+                System.nanoTime() - start);
+      }
     }
 
     if (blockLog.isDebugEnabled()) {
